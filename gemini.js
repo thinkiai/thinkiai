a0587987286c7c0f74f6242bf0fd5def4979dbdc
thinkiai <divanonetheless@gmail.com>
2025-07-13 11:08:45 -0500

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -1,4 +1,4 @@
-imporimportt { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
